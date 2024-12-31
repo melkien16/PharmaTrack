c6de01e7aed03ba5a4a75204bb8ffea69103df26
melkie <melkieyilk@gmail.com>
2025-01-01 00:10:59 +0300

    change the main image

--- src/components/main/Feauture.jsx
+++ src/components/main/Feauture.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Img from "/images/pharmacist.jpg";
+import Img from "/images/pm2.jpg";
 import Img1 from "/images/filter.png";
 import Img2 from "/images/map.png";
 import Img3 from "/images/medical.png";
@@ -14,14 +14,14 @@ const Feauture = () => {
       <div className="relative">
         <img src={Img} alt="" width={950} className="rounded-lg shadow-lg" />
         <div className="absolute top-0 right-0 flex flex-col items-center justify-between p-4 rounded-lg space-y-6 h-full">
-          <div className="w-full bg-primary-100 p-6 px-14 rounded-lg shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
+          <div className="w-full bg-primary-100 p-6 px-14 rounded-full shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
             <img src={Img1} alt="" className="w-12 h-12" />
             <div>
               <h2 className="text-2xl font-bold mb-2">Search and Filter</h2>
               <p className="text-lg mb-4">Search and filter pharmacies by distance</p>
             </div>
           </div>
-          <div className="w-full bg-primary-100 p-6 px-14 rounded-lg shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
+          <div className="w-full bg-primary-100 p-6 px-14 rounded-full shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
             <img src={Img2} alt="" className="w-12 h-12" />
             <div>
               <h2 className="text-2xl font-bold mb-2">Locate on Map</h2>
@@ -30,14 +30,14 @@ const Feauture = () => {
               </p>
             </div>
           </div>
-          <div className="w-full bg-primary-100 p-6 px-14 rounded-lg shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
+          <div className="w-full bg-primary-100 p-6 px-14 rounded-full shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
             <img src={Img3} alt="" className="w-12 h-12" />
             <div>
               <h2 className="text-2xl font-bold mb-2">Drug Information</h2>
               <p className="text-lg mb-4">Get drug information</p>
             </div>
           </div>
-          <div className="w-full bg-primary-100 p-6 px-14 rounded-lg shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
+          <div className="w-full bg-primary-100 p-6 px-14 rounded-full shadow-xl hover:bg-primary-200 transition duration-300 flex items-center space-x-10 hover:scale-110">
             <img src={Img4} alt="" className="w-12 h-12" />
             <div>
               <h2 className="text-2xl font-bold mb-2">Pharmacy informarions</h2>
