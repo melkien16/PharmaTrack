aa940ca6b78f3a74ac19e490d3524cc13d33c12b
melkie <melkieyilk@gmail.com>
2025-01-01 09:31:51 +0300

    import footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Home from "./components/main/Home";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <div className="">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 };
